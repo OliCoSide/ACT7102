fba80a040d31302163e53a1796dc99dce68dfe43
Benjamin Côté <benjamincote00@gmail.com>
2022-02-28 15:10:53 -0500

    Ajout propo 9 10 11

--- Exemple7_Frank.R
+++ Exemple7_Frank.R
@@ -1,4 +1,4 @@
-  ###Exemple 7 de Cossette et al. (2011)
+###Exemple 7 de Cossette et al. (2011)
 
 densM1 <- function(xx) dpois(xx, lambda = 4)
 densM2 <- function(xx) dnbinom(xx, size = 4, prob = 0.5)
@@ -60,23 +60,51 @@ repX2 <- function(x) densM2(0)+sum(densM2(1:500)*pgamma(x,shape = (1:500)*0.25,r
 VaRkX1 <- function(k) optimize(function(x,k) abs(repX1(x)-k), c(0,500), k=k)$minimum
 VaRkX2 <- function(k) optimize(function(x,k) abs(repX2(x)-k), c(0,500), k=k)$minimum
 
-TVaRkX1 <- function(k) 1/(1-k)*sum(densM1(0:500)*(0:500)*0.5/0.1*pgamma(VaRkX1(k),(0:500)*0.5+1,0.1))
-TVaRkX2 <- function(k) 1/(1-k)*sum(densM2(0:500)*(0:500)*0.25/0.1*pgamma(VaRkX2(k),(0:500)*0.25+1,0.1))
+TVaRkX1 <- function(k) 1/(1-k)*sum(densM1(0:500)*(0:500)*0.5/0.1*(1-pgamma(VaRkX1(k),(0:500)*0.5+1,0.1)))
+TVaRkX2 <- function(k) 1/(1-k)*sum(densM2(0:500)*(0:500)*0.25/0.1*(1-pgamma(VaRkX2(k),(0:500)*0.25+1,0.1)))
 
                                    
 table2 <- data.frame(kappa = NULL, VaRX1 = NULL, VaRX2 = NULL, TVaRX1 = NULL, TVaRX2 = NULL, sumVaR = NULL, sumTVaR = NULL)
 
 for (k in c(0.25,0.50,0.95,0.99,0.995)){
-  VaRX1 <- VaRkX1(k)
-  VaRX2 <- VaRkX2(k)
-  TVaRX1 <- TVaRkX1(k)
-  TVaRX2 <- TVaRkX2(k)
-  sumVaR <- VaRX1 + VaRX2
-  sumTVaR <- TVaRX1 + TVaRX2
+  VaRX1 <- round(VaRkX1(k),4)
+  VaRX2 <- round(VaRkX2(k),4)
+  TVaRX1 <- round(TVaRkX1(k),4)
+  TVaRX2 <- round(TVaRkX2(k),4)
+  sumVaR <- round(VaRX1 + VaRX2,4)
+  sumTVaR <- round(TVaRX1 + TVaRX2,4)
   ttt <- data.frame(kappa = k, VaRX1 = VaRX1, VaRX2 = VaRX2, TVaRX1 = TVaRX1, TVaRX2 = TVaRX2, sumVaR = sumVaR, sumTVaR = sumTVaR)
   table2 <- rbind(table2, ttt)
 }
 table2
 
+in.repS <- function(x, m1,m2, alpha) densM1M2(m1,m2,alpha)*pgamma(x, shape = (0.5*m1+0.25*m2), rate = 0.1)
+in.TVaRkS <- function(x, m1,m2, alpha) densM1M2(m1,m2,alpha)*(m1*0.5+m2*0.25)/0.1*(1-pgamma(VaRS, shape = (0.5*m1+0.25*m2), rate = 0.1))
+in.TVaRkX1.S <-  function(x, m1,m2, alpha) densM1M2(m1,m2,alpha)*(m1*0.5)/0.1*(1-pgamma(VaRS, shape = (0.5*m1+0.25*m2), rate = 0.1))
+in.TVaRkX2.S <- function(x, m1,m2, alpha) densM1M2(m1,m2,alpha)*(m2*0.25)/0.1*(1-pgamma(VaRS, shape = (0.5*m1+0.25*m2), rate = 0.1))
 
+repS <- function(x, alpha) sum(outer((0:500), (0:500), in.repS, x=x, alpha=alpha))
 
+VaRkS <- function(k, alpha) optimize(function(x,k,alpha) abs(repS(x, alpha)-k), c(5,200), k=k, alpha=alpha)$minimum
+
+TVaRkS <- function(k, alpha) 1/(1-k)*sum(outer((0:500), (0:500), in.TVaRkS, x=x, alpha=alpha))
+
+TVaRkX1.S <- function(k, alpha) 1/(1-k)*sum(outer((0:500), (0:500), in.TVaRkX1.S, x=x, alpha=alpha))
+
+TVaRkX2.S <- function(k, alpha) 1/(1-k)*sum(outer((0:500), (0:500), in.TVaRkX2.S, x=x, alpha=alpha))
+
+table3 <- data.frame(alpha = NULL, kappa = NULL, VaRS = NULL, TVaRS = NULL, TVaRX1.S = NULL, TVaRX2.S = NULL)
+
+for (alph in c(-20,0,20)){
+for (k in c(0.25,0.50,0.95,0.99,0.995)){
+  VaRS <- VaRkS(k, alph)
+  TVaRS <- TVaRkS(k, alph)
+  TVaRX1.S <- TVaRkX1.S(k, alph)
+  TVaRX2.S <- TVaRkX2.S(k, alph)
+  ttt <- data.frame(alpha = alph, kappa = k, VaRS = VaRS, TVaRS = TVaRS, TVaRX1.S = TVaRX1.S, TVaRX2.S = TVaRX2.S)
+  table3 <- rbind(table3, ttt)
+}
+}
+##Temps de calcul est long
+
+table3
